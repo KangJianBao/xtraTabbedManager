fc72b3459443be26bbd70d55732255fa46f3510d
TaKaSa <takasa@qq.com>
2019-03-12 14:26:39 +0800

    UI

--- DXApplication1/Form1.cs
+++ DXApplication1/Form1.cs
@@ -15,5 +15,21 @@ namespace DXApplication1
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create a BarManager that will display a bar of commands at the top of the main form.
+        
+        }
+
+        int ctr = 0;
+        
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Text = "Child Form " + (++ctr).ToString();
+            f.MdiParent = this;
+            f.Show();
+        }
     }
 }
